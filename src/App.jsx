ae03c4441d60284d6f6b2277f48403a5a2da35d9
Sravan <sravan22495@gmail.com>
2026-04-11 15:29:26 +0530

    feat: login UI setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router"
-import Body from "./Body"
-import Login from "./Login"
-import Profile from "./Profile" 
+import Body from "../components/Body"
+import Login from "../components/Login"
+import Profile from "../components/Profile" 
+import Feed from "../components/Feed"
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter basename="/">
     <Routes>
       <Route path="/" element={<Body />} >
+      <Route path="/" element={<Feed />} />
       <Route path="/login" element={<Login />} />
       <Route path="/profile" element={<Profile />} />
       </Route>
